b03f32bcb984503726751b7d564a8201322d9430
Cesaire <G00398843@gmit.ie>
2021-02-13 13:18:44 +0000

    Lab3.1.4-randomGenerator.py

---
+++ week03/Lab3.1.4-randomGenerator.py
@@ -0,0 +1,8 @@
+# Author: Cesaire Tchoudjuen
+# Program that prints out a random number between 1 and 10
+
+import random
+
+number = random.randrange(1, 10)
+
+print("Here is a random number: {}".format(number))
