86730f303f9975da2c1a17eb1ea013a868408af7
Cesaire <G00398843@gmit.ie>
2021-04-14 21:42:36 +0100

    weeklytasks

--- WeeklyTasks/weeklyTask08-plottask.py
+++ WeeklyTasks/weeklyTask08-plottask.py
@@ -5,16 +5,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.array(range(1,5))
+# We define below our 3 functions f(x)=x, g(x)=x² and h(x)=x³
 fFunction = x
 gFunction = x * x
 hFunction = x * x * x
 
 
-
+# For each function, we generate a graph to visualize it with x = [0,4]
 
 plt.plot(x, fFunction, label = "f(x) = x")
 plt.plot(x, gFunction, label = "g(x) = x²")
 plt.plot(x, hFunction, label = "h(x) = x³")
+# We add a title to our graph as well as a legend
 plt.title("Plot Task: Plot of functions f(x)=x, g(x)=x² and h(x)=x³")
 plt.legend()
 
