8c065219bebee1ab9aa68131c7370331215182dd
Cesaire <G00398843@gmit.ie>
2021-04-14 19:36:09 +0100

    programming

--- Week05-Datastructures/WeeklyTask5-weekday.py
+++ Week05-Datastructures/WeeklyTask5-weekday.py
@@ -8,7 +8,9 @@ weekend_days = ['Saturday', 'Sunday'] # List of weekend days
 x = datetime.datetime.now() # variable assigned to an object returning the current date and time
 today = x.strftime("%A") # The current date and time format is changed to only return the current day of the week
 
+
 if today in week_days: # we check if the current day of the week is included in the week days
     print("Yes, unfortunately today is a weekday.")
 else: # we check if the current day of the week is included in the weekend days
     print('It is the weekend, yay!')
+
