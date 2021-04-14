fe13d7123999b7c45e7286ac49670dcf3d4ff616
Cesaire <G00398843@gmit.ie>
2021-04-14 21:20:53 +0100

    programming

--- week07-files/weeklyTask07-es.py
+++ week07-files/weeklyTask07-es.py
@@ -3,17 +3,17 @@
 
 
 # Program asks user to enter the file name
-# File should be stored in the same directory as the program itsel
+# File should be stored in the same directory as the program itself. If not, please change directory from the console
 file_name = input("Please enter the file name: ")
 letter_input = input("Please enter the letter to be counted: ")
-count = 0
+count = 0 #We initiate the letter count to 0. Later, the program will increment it as it count the target letter in the selected file
 
 
 with open(file_name, 'r') as f: # Opens the designated file in the read mode
-    for i in f:
-        word = i.split() # split() method splits a string into a list
-        for j in word:
-            for letter in j:
+    for i in f: # The program will scan the entire file
+        word = i.split() # split() method splits a string into a list of words
+        for j in word: # Now the program will scan each word
+            for letter in j: # In each word from the list created above, if the target letetr is found, then our count is increased by 1
                 if(letter == letter_input):
                     count = count + 1
 
